2078c5893dba2a2b38a54040aecf00206eb8af9d
Didarul Alam Sourov <da.sourov.11@gmail.com>
2025-09-11 17:32:53 +0200

    Fix API

--- pages/payment.js
+++ pages/payment.js
@@ -42,7 +42,7 @@ export default function PaymentPage() {
     };
 
     // Send order to FastAPI backend
-    const response = await fetch("http://127.0.0.1:8000/api/orders/", {
+    const response = await fetch("https://spyrobackend.onrender.com/api/orders/", {
       method: "POST",
       headers: { "Content-Type": "application/json" },
       body: JSON.stringify(orderDetails),
